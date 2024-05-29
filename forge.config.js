0ba8a7c33d3dc9dd21a5791a2ee88d526c94b058
Xie Zhihao <xzh1206@gmail.com>
2024-05-29 19:54:49 +0800

    Build exe and dmg

--- forge.config.js
+++ forge.config.js
@@ -5,7 +5,18 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        format: "ULFO",
+      },
+    },
+    {
       name: "@electron-forge/maker-zip",
+      platforms: [],
     },
   ],
 };
