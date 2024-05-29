ae57b1b7e5021d131673ab8d370ebb8a178f9539
Xie Zhihao <xzh1206@gmail.com>
2024-05-29 20:46:03 +0800

    Build dmg (#3)

--- forge.config.js
+++ forge.config.js
@@ -5,7 +5,15 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        name: "PCRN",
+        format: "ULFO",
+      },
+    },
+    {
       name: "@electron-forge/maker-zip",
+      platforms: ["win32"],
     },
   ],
 };
